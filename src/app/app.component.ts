554d07a15ecb2cace890679e5a38ab318600e226
Johannes Baum <baum@bw-webgestaltung.de>
2019-09-11 11:24:51 +0200

    add sound toggling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Howl, Howler } from "howler";
+import { MatSlideToggleModule } from "@angular/material/slide-toggle";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
-export class AppComponent {
-  title = 'hack19-istio-ui';
+export class AppComponent implements OnInit {
+  title = "hack19-istio-ui";
+  sounds = {};
+
+  ngOnInit() {
+    this.sounds["communicator"] = {
+      sound: new Howl({
+        src: ["http://localhost:3000/"],
+        format: ["mp3"],
+        loop: true
+      }),
+      loop: false
+    };
+    this.sounds["wololo"] = {
+      sound: new Howl({
+        src: ["http://localhost:3000/wololo"],
+        format: ["mp3"],
+        loop: true
+      }),
+      loop: false
+    };
+  }
+
+  playLoop(name) {
+    this.sounds[name].loop
+      ? this.sounds[name].sound.stop()
+      : this.sounds[name].sound.play();
+    this.sounds[name].loop = !this.sounds[name].loop;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,13 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { MatSlideToggleModule } from "@angular/material/slide-toggle";
 
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, MatSlideToggleModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
