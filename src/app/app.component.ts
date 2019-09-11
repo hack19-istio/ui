699febe9285f6879055321966f4419581a1b19dc
Johannes Baum <baum@bw-webgestaltung.de>
2019-09-11 16:26:59 +0200

    visualization

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,12 +13,12 @@ export class AppComponent implements OnInit {
   soundList = [
     "rhodes1",
     "bass1",
+    "bass2",
     "hh1",
-    "kickSnare1",
+    "kicksnare1",
     "pad1",
     "piano1",
-    "piano2",
-    "asd"
+    "piano2"
   ];
 
   constructor(private http: HttpClient) {}
@@ -27,7 +27,7 @@ export class AppComponent implements OnInit {
     this.soundList.forEach(soundName => {
       this.sounds[soundName] = {
         sound: new Howl({
-          src: [`http://localhost:3000/instrument-file?name=${soundName}`],
+          src: [`http://35.188.57.69/instrument-file?name=${soundName}`],
           format: ["mp3"],
           loop: true
         }),
@@ -35,6 +35,7 @@ export class AppComponent implements OnInit {
         offline: false
       };
     });
+    this.ctxMagic();
   }
 
   playLoop(name) {
@@ -53,7 +54,7 @@ export class AppComponent implements OnInit {
       this.sounds[name].offline = true;
     };
     this.http
-      .get(`http://localhost:3000/instrument-file?name=${name}`, {
+      .get(`http://35.188.57.69/instrument?name=${name}`, {
         responseType: "blob"
       })
       .subscribe(onSuccess, onError);
@@ -92,8 +93,72 @@ export class AppComponent implements OnInit {
     Object.keys(this.sounds).forEach(key => {
       if (this.sounds[key].play) {
         this.sounds[key].sound.play();
+
         this.sounds[key].sound.on("end", () => console.log(key + " finished"));
       }
     });
+  }
+
+  private ctxMagic() {
+    // Create analyzer
+    var analyser = Howler.ctx.createAnalyser();
+
+    // Connect master gain to analyzer
+    Howler.masterGain.connect(analyser);
+
+    // Connect analyzer to destination
+    analyser.connect(Howler.ctx.destination);
+
+    // Creating output array (according to documentation https://developer.mozilla.org/en-US/docs/Web/API/Web_Audio_API/Visualizations_with_Web_Audio_API)
+    analyser.fftSize = 2048;
+    var bufferLength = analyser.frequencyBinCount;
+    var dataArray = new Uint8Array(bufferLength);
+
+    // Get the Data array
+    analyser.getByteTimeDomainData(dataArray);
+
+    // Display array on time each 3 sec (just to debug)
+    setInterval(function() {
+      analyser.getByteTimeDomainData(dataArray);
+      console.dir(dataArray);
+    }, 3000);
+
+    var canvas = document.getElementById("oscilloscope");
+    var canvasCtx = canvas.getContext("2d");
+
+    function draw() {
+      requestAnimationFrame(draw);
+
+      analyser.getByteTimeDomainData(dataArray);
+
+      canvasCtx.fillStyle = "rgb(255, 255, 255)";
+      canvasCtx.fillRect(0, 0, canvas.width, canvas.height);
+
+      canvasCtx.lineWidth = 2;
+      canvasCtx.strokeStyle = "rgb(0, 0, 0)";
+
+      canvasCtx.beginPath();
+
+      var sliceWidth = (canvas.width * 1.0) / bufferLength;
+      var x = 0;
+
+      for (var i = 0; i < bufferLength; i++) {
+        var v = dataArray[i] / 128.0;
+        var y = (v * canvas.height) / 2;
+
+        if (i === 0) {
+          canvasCtx.moveTo(x, y);
+        } else {
+          canvasCtx.lineTo(x, y);
+        }
+
+        x += sliceWidth;
+      }
+
+      canvasCtx.lineTo(canvas.width, canvas.height / 2);
+      canvasCtx.stroke();
+    }
+
+    draw();
   }
 }
