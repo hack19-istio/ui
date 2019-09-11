53f789d7507fbfe86254084fa7498b1e2643947f
Johannes Baum <baum@bw-webgestaltung.de>
2019-09-11 13:44:40 +0200

    make ui great again

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,21 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { MatSlideToggleModule } from "@angular/material/slide-toggle";
+import { AngularFontAwesomeModule } from "angular-font-awesome";
+import { MatCardModule } from "@angular/material/card";
+import { MatDividerModule } from "@angular/material/divider";
 
 import { AppComponent } from "./app.component";
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, MatSlideToggleModule],
+  imports: [
+    BrowserModule,
+    MatSlideToggleModule,
+    AngularFontAwesomeModule,
+    MatCardModule,
+    MatDividerModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
