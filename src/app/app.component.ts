08d4c2ce5e072a2fe1dc58668867c06db3cfb57a
Joerg Wuethrich <joerg.wuethrich@axa-winterthur.ch>
2019-09-11 19:30:44 +0200

    add 2nd kubernetes clusterhost

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,11 @@ import { Component, OnInit } from "@angular/core";
 import { Howl, Howler } from "howler";
 import { HttpClient } from "@angular/common/http";
 
+// istio-k8s-cluster-1
+// const clusterHost = '35.188.57.69';
+// istio-k8s-cluster-2
+const clusterHost = '35.226.139.104';
+
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
@@ -27,7 +32,7 @@ export class AppComponent implements OnInit {
     this.soundList.forEach(soundName => {
       this.sounds[soundName] = {
         sound: new Howl({
-          src: [`http://35.188.57.69/instrument-file?name=${soundName}`],
+          src: [`http://${clusterHost}/instrument-file?name=${soundName}`],
           format: ["mp3"],
           loop: true
         }),
@@ -54,7 +59,7 @@ export class AppComponent implements OnInit {
       this.sounds[name].offline = true;
     };
     this.http
-      .get(`http://35.188.57.69/instrument?name=${name}`, {
+      .get(`http://${clusterHost}/instrument?name=${name}`, {
         responseType: "blob"
       })
       .subscribe(onSuccess, onError);
